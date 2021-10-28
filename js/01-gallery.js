afa85d70f7b471e6d05602dec2bfb8dc04b12c13
Sofia Protsiv <prochivsofia@gmail.com>
2021-10-28 04:10:20 -0400

    completed 2 tasks

---
+++ js/01-gallery.js
@@ -0,0 +1,62 @@
+"use strict";
+
+import { galleryItems } from './gallery-items.js';
+
+const imgContainer = document.querySelector('.gallery');
+let preview = {};
+
+let galleryMarkup = createMarkup(galleryItems);
+imgContainer.innerHTML = galleryMarkup;
+
+imgContainer.addEventListener('click', onClickImg);
+
+function createMarkup(galleryData) {
+    return galleryData.map(({preview, original, description}) => {
+      return `
+            <div class="gallery__item">
+              <a class="gallery__link" href="${original}">
+                <img
+                  class="gallery__image"
+                  src="${preview}"
+                  data-source="${original}"
+                  alt="${description}"
+                />
+              </a>
+            </div>
+            `
+    }).join('');  
+  };
+
+function onClickImg (event){
+    event.preventDefault();
+
+    if (event.target.nodeName !== 'IMG') {
+        return
+      }
+  
+  createBasicLightbox(event.target);
+  modalOpen(preview);
+}
+
+function createBasicLightbox(img) {  
+  preview = basicLightbox.create(`
+      <img src="${img.getAttribute('data-source')}" width="800" height="600">
+      `);
+};
+
+function modalOpen() {
+  preview.show();
+  window.addEventListener('keydown', closeOnEscPress)  
+};
+
+function closeOnEscPress(event) {
+  if (event.code !== 'Escape') {
+    return;
+  }
+  preview.close();
+  window.removeEventListener('keydown', closeOnEscPress);
+};
+
+
+
+
---
+++ js/02-lightbox.js
@@ -0,0 +1,19 @@
+  'use strict';
+
+import { galleryItems } from './gallery-items.js';
+
+const imgContainer = document.querySelector('.gallery');
+let galleryMarkup = createMarkup(galleryItems);
+
+imgContainer.innerHTML = galleryMarkup;
+
+function createMarkup(galleryData) {
+  return galleryData.map(({preview, original, description}) => {
+      return `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`
+  }).join('');  
+};
+
+const modal = new SimpleLightbox('.gallery a', { captions: true, captionsData: 'alt', captionDelay: 200 });
